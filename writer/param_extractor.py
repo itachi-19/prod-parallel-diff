cfdb679198bd6bc53f57da0431293e230c710289
itachi-19 <sulabh.biswas.0157@gmail.com>
2022-04-11 18:16:32 +0530

    Param extraction bug fix

--- writer/param_extractor.py
+++ writer/param_extractor.py
@@ -7,11 +7,11 @@ class ParamExtractor:
 
     def get_icapture_vals(self):
         vals = [ diff[1][0] for diff in self.diffs ]
-        return '<br>'.join(vals)
+        return '<br>'.join(map(str,vals))
 
     def get_fidstp_vals(self):
         vals = [ diff[1][1] for diff in self.diffs ]
-        return '<br>'.join(vals)
+        return '<br>'.join(map(str,vals))
 
     def get_xpaths(self):
         xpaths = [ diff[0] for diff in self.diffs ]
